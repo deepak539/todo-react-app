d5c1b1a1755c96198c1beb740e0f86b04d40db2c
deepak539 <deepakasnani167@gmail.com>
2021-01-06 11:28:24 +0530

    My first react project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TodoList from './components/TodoList';
+//import TodoForm from './components/TodoForm';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
